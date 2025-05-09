818f33f7060bfc8536ee253127126352e11da84a
unknown <241105>
2025-05-09 11:24:26 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
+// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+public class Main {
+    public static void main(String[] arg){
+        System.out.print("栗本　流碧");
+        for(int i = 1; i<= 5; i++){
+            System.out.println("i = " + i);
+        }
+    }
+}
