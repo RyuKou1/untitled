db7b02d17b1980e06f18bd3564e85e4b49fd2609
241105 <241105@std.hi-joho.ac.jp>
2025-05-16 11:16:54 +0900

    githubにあげるテスト

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] arg){
         System.out.print("栗本　流碧");
+        System.out.println("変更を加えた");
         for(int i = 1; i<= 5; i++){
             System.out.println("i = " + i);
         }
---
+++ src/test.java
@@ -0,0 +1,5 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("出力したい文字を書き込む");
+    }
+}
