afeac28f3c92bead1bd92ec7f1658bac1f9bdf14
241105 <241105@std.hi-joho.ac.jp>
2025-05-21 10:43:28 +0900

    課題

---
+++ src/Review12.java
@@ -0,0 +1,14 @@
+public class Review12 {
+    public static void main(String[] args) {
+        int age = 0;
+        if (age < 0) {
+            System.out.println("無効な年齢です。");
+        } else if (age < 18) {
+            System.out.println("未成年");
+        } else if (age < 65) {
+            System.out.println("成人");
+        } else {
+            System.out.println("高齢者");
+        }
+    }
+}
