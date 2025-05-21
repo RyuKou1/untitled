2361c627daab43abdce6f526c50b01e68c154092
241105 <241105@std.hi-joho.ac.jp>
2025-05-21 10:08:24 +0900

    githubにあげるテスト

---
+++ src/Review4.java
@@ -0,0 +1,14 @@
+public class Review4 {
+    public static void main(String[] args) {
+        int number1 = 5;
+        String str = 88 +"88";
+        final double PI = 3.141592;
+        double pi = 3.14;
+        float circ = 3.14f;
+        long a = 10000000000L;
+        boolean flg = true;
+        byte b = 35;
+        int num = 735;
+        char c = 'a';
+    }
+}
