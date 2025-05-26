58d15742852d0ba9b16617581d84bda7b683e47c
241105 <241105@std.hi-joho.ac.jp>
2025-05-26 11:44:24 +0900

    課題

--- src/Review28.java
+++ src/Review28.java
@@ -1,5 +1,14 @@
+import java.io.*;
 public class Review28  {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("宛先を入力>");
+        String to = br.readLine();
+        System.out.print("件名を入力>");
+        String subject = br.readLine();
+        System.out.print("本文を入力>");
+        String body = br.readLine();
+
 
     }
 }
