0bda87fe669dcf37190a7c51e6f1ab3c8f3d9bf8
241105 <241105@std.hi-joho.ac.jp>
2025-05-26 11:35:24 +0900

    課題

---
+++ src/Review28.java
@@ -0,0 +1,5 @@
+public class Review28  {
+    public static void main(String[] args) {
+
+    }
+}
--- src/reviews/reviews2/Review22.java
+++ src/reviews/reviews2/Review22.java
@@ -1,11 +1,11 @@
 package reviews.reviews2;
 
-public class Reviews21 {
+public class Review22 {
     public static void main(String[] args) {
-        int num = 4;
+        int num = 5;
         int sum = 0;
         for (int i = 0; i <= num; i++) {
-            if (i % 2 == 0) sum += i;
+            if(i % 2 == 1) sum += i;
         }
         System.out.println("結果: " + sum);
     }
