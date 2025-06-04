8aac9509fc8ca2c9ef7472f3c7a84f686f93ac06
241105 <241105@std.hi-joho.ac.jp>
2025-06-04 10:43:27 +0900

    課題

---
+++ src/textbook_practice/chapter1/Practice3.java
@@ -0,0 +1,11 @@
+package textbook_practice.chapter1;
+
+public class Practice3 {
+    public static void main(String[] args) {
+        boolean a = true;
+        char b = '駆';
+        float c = 3.14f;
+        long d = 3145159265853979L;
+        String e = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+    }
+}
