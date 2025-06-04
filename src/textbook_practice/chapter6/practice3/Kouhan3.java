13e81891fabc91d2b97b333a28a01153584b1aad
241105 <241105@std.hi-joho.ac.jp>
2025-06-04 10:35:10 +0900

    課題

--- src/textbook_practice/chapter6/practice3/Kouhan3.java
+++ src/textbook_practice/chapter6/practice3/Kouhan3.java
@@ -1,12 +1,15 @@
 package textbook_practice.chapter6.practice3;
 
+import static java.lang.Thread.sleep;
+
 public class Kouhan3 {
     public static void callDeae() {
         System.out.println("えぇい、こしゃくな。くせ者だ！であえい。");
     }
-    public static void showMondokoro() {
+    public static void showMondokoro() throws InterruptedException {
         System.out.println("飛車さん、角さん。もういいでしょう。");
         System.out.println("この紋所が目にはいらぬか！");
+        sleep(3000);
         Zenhan3.doTogame();
     }
 }
